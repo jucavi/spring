229f891b1f1bb65e7df23369463e0127882d5edd
Jucavi <jcarvil@gmail.com>
2024-03-18 13:15:35 +0100

    init spring rest data jpa

---
+++ ob-04-rest-datajpa/src/main/java/com/example/ob04restdatajpa/Ob04RestDatajpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ob04restdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ob04RestDatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ob04RestDatajpaApplication.class, args);
+	}
+
+}
---
+++ ob-04-rest-datajpa/src/test/java/com/example/ob04restdatajpa/Ob04RestDatajpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ob04restdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ob04RestDatajpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
